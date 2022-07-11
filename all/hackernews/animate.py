77e737c11b3a4b594d0f6a6bd00f825d8a97e527
Jon Udell <judell@turbot.com>
2022-07-11 10:29:07 -0700

    fix animation demo

--- all/hackernews/animate.py
+++ all/hackernews/animate.py
@@ -1,9 +1,8 @@
 import re, time
 
-arg_pattern = r'(args = )([^d]+)(\d+)(, )(\d+)( ] // companies)'
+arg_pattern = r'(args = \[ local.companies, )(\d+)(, )(\d+)'
 
-
-title_pattern = r'(company mentions: )(\d+)( to )(\d+)( hours ago)'
+title_pattern = '(company mentions: )(\d+)( to )(\d+)( hours ago)'
 
 max = 60 * 24 * 5
 
@@ -14,26 +13,18 @@ def shift():
     s = f1.read()
     a_group = re.search(arg_pattern, s).group()
     a_groups = re.search(arg_pattern, s).groups()
-    a0 = a_groups[0]
-    a1 = a_groups[1]
-    a2 = int(a_groups[2])
-    a3 = a_groups[3]
-    a4 = int(a_groups[4])
-    a5 = a_groups[5]
-    a2 = a2 - 60 * hours_ago
-    a4 = a2 - 60 * hours_ago
-    if a4 < 0:
-      a2 = max
-      a4 = a2 - 60 * hours_ago
-    a_group2 = f'{a0}{a1}{a2}{a3}{a4}{a5}'
+    a1 = int(a_groups[1])
+    a3 = int(a_groups[3])
+    a1 = a1 - 60 * hours_ago
+    a3 = a3 - 60 * hours_ago
+    if a3 < 0:
+      a1 = max
+      a3 = a1 - 60 * hours_ago
+    a_group2 = f'args = [ local.companies, {a1}, {a3}'
     s = s.replace(a_group, a_group2)
 
     t_group = re.search(title_pattern, s).group()
-    t_groups = re.search(title_pattern, s).groups()
-    t0 = t_groups[0]
-    t2 = t_groups[2]
-    t4 = t_groups[4]
-    t_group2 = f'{t0}{int(a2/60)}{t2}{int(a4/60)}{t4}'
+    t_group2 = f'company mentions: {int(a1/60)} to {int(a3/60)} hours ago'
     s = s.replace(t_group, t_group2)
 
   with open('animation.sp', 'w') as f2:
--- all/hackernews/animation.sp
+++ all/hackernews/animation.sp
@@ -11,9 +11,9 @@ dashboard "AnimatedCompanyMentions" {
       base = chart.companies_base
       width = 8
       type = "donut"
-      title = "company mentions: 72 to 60 hours ago" // companies
+      title = "company mentions: 84 to 72 hours ago" 
       query = query.mentions
-      args = [ local.companies, 4320, 3600 ] // companies
+      args = [ local.companies, 5040, 4320 ] 
     }
 
     text {

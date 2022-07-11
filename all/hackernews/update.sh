16184524c532d2d22e1974da7d092d42663834d7
Jon Udell <judell@turbot.com>
2022-07-11 07:51:28 -0700

    10 -> 5, fix null->0

--- all/hackernews/update.sh
+++ all/hackernews/update.sh
@@ -35,8 +35,8 @@ steampipe query "update hn_items_all set descendants = 0::text where descendants
 
 echo 'create table hn_scores_and_comments'
 steampipe query "drop table if exists hn_scores_and_comments"
-steampipe query "create table public.hn_scores_and_comments as select id, score, descendants from hn_items_all where score::int > 10 "
-#steampipe query "create index idx_hn_scores_and_comments_id on public.hn_scores_and_comments(id)"
+steampipe query "create table public.hn_scores_and_comments as select id, score, descendants from hn_items_all where score::int > 5 "
+steampipe query "create index idx_hn_scores_and_comments_id on public.hn_scores_and_comments(id)"
 
 echo 'update hn_items_all from new_sc'
 cp new_sc.csv ~/csv/new_sc.csv
@@ -51,11 +51,9 @@ steampipe query <<EOQ
   where new_sc.id = a.id
 EOQ
 
-echo 'set null comments to 0'
 steampipe query "update hn_items_all set descendants = 0::text where descendants = '<null>'"
 
-echo 'set null scores to 0'
-steampipe query "update hn_items_all set descendants = 0::text where descendants = '<null>'"
+steampipe query "update hn_items_all set scores = 0::text where scores = '<null>'"
 
 
  

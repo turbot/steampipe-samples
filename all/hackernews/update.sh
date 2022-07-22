6d5cbb04d0974e8cc058950c3d66414b5a6b987d
Jon Udell <judell@turbot.com>
2022-07-22 13:16:20 -0700

    use hn_items_all.csv, quieter output

--- all/hackernews/update.sh
+++ all/hackernews/update.sh
@@ -1,53 +1,25 @@
 echo 'git pull'
-
-git pull
-
-cat hn_header.txt > hn.csv
-
-echo 'combine csv files'
-
-for file in ./csv/hn_*.csv; do
-  tail -n +2 $file >> hn.csv
-done
-
-mv hn.csv ~/csv
-
-echo 'drop table if exists hn_items_tmp'
-steampipe query "drop table if exists public.hn_items_tmp"
-
-echo 'create table hn_items_tmp'
-steampipe query "create table public.hn_items_tmp as select * from csv.hn"
-
-echo 'drop table if exists hn_items_all'
-steampipe query "drop table if exists public.hn_items_all"
+git pull 2>&1
 
 echo 'create table hn_items_all'
-steampipe query "create table public.hn_items_all as select distinct on (id) * from hn_items_tmp"
+cp hn_items_all.csv ~/csv >/dev/null 2>&1
+steampipe query "drop table if exists hn_items_all" >/dev/null 2>&1
+steampipe query "create table public.hn_items_all as select distinct on (id) * from csv.hn_items_all" >/dev/null 2>&1
 
 echo 'create index'
-steampipe query "create index idx_hn_items_all_by on public.hn_items_all(by)"
+steampipe query "create index idx_hn_items_all_by on public.hn_items_all(by)" >/dev/null 2>&1
 
-echo 'set null comments to 0'
-steampipe query "update hn_items_all set descendants = 0::text where descendants = '<null>'"
-
-echo 'create table hn_scores_and_comments'
-steampipe query "drop table if exists hn_scores_and_comments"
-steampipe query "create table public.hn_scores_and_comments as select id, score, descendants from hn_items_all where score::int >= 5 "
-steampipe query "create index idx_hn_scores_and_comments_id on public.hn_scores_and_comments(id)"
-
-echo 'update hn_items_all from new_sc'
-cp new_sc.csv ~/csv/
-steampipe query query.update_scores_and_comments
-
-steampipe query "update hn_items_all set descendants = 0::text where descendants = '<null>'"
-steampipe query "update hn_items_all set score = 0::text where score = '<null>'"
-steampipe query "update hn_items_all set url = '' where url = '<null>'"
-
+echo 'cast types'
+steampipe query "update hn_items_all set descendants = 0::text where descendants = '<null>'" >/dev/null 2>&1
+steampipe query "update hn_items_all set descendants = 0::text where descendants = '<null>'" >/dev/null 2>&1
+steampipe query "update hn_items_all set score = 0::text where score = '<null>'" >/dev/null 2>&1
+steampipe query "update hn_items_all set url = '' where url = '<null>'" >/dev/null 2>&1
 
 echo 'create table hn_people'
-cp people.csv ~/csv/
-steampipe query "drop table if exists hn_people"
-steampipe query "create table public.hn_people as select * from csv.people"
+cp people.csv ~/csv/ >/dev/null 2>&1
+steampipe query "drop table if exists hn_people" >/dev/null 2>&1
+steampipe query "create table public.hn_people as select * from csv.people" >/dev/null 2>&1
 
+echo 'now run `steampipe dashboard`, then visit http://localhost:9194 and check out the hacker news dashboard!'
 
  
